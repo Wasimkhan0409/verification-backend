4f4e5f626e2450c2dd767eb96a2941c6d18e2b05
QudratUllahKhan9 <qudratkhan0409@gmail.com>
2025-04-13 00:45:11 -0700

    o

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS settings
 const corsOptions = {
-  origin: ['https://fortesting1212.netlify.app', 'http://localhost:5000'],
+  origin: ['https://fortesting1212.netlify.app', 'http://localhost:3000'],
   methods: ['GET'],
   allowedHeaders: ['Content-Type'],
 };
