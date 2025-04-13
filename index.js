b724f6d0fcad17f0bce4ac2baac7df6ea6194136
QudratUllahKhan9 <qudratkhan0409@gmail.com>
2025-04-13 01:07:46 -0700

    o

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS settings
 const corsOptions = {
-  origin: ['https://fortesting1212.netlify.app', 'http://localhost:3000'],
+  origin: ['https://qrcode-vert-six.vercel.app/', 'http://localhost:3000'],
   methods: ['GET'],
   allowedHeaders: ['Content-Type'],
 };
