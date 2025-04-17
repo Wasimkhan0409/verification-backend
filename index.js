225df0fd080583064fee29cb0e38cfe4dbc49c26
Qudrat Ullah Khan <qudratkhan0409@gmail.com>
2025-04-17 02:08:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'www.bureauveratas.com', // or http://localhost:3000
+  origin: 'https://www.bureauveratas.com', // or http://localhost:3000
   methods: ['GET'],
   credentials: true,
 };
